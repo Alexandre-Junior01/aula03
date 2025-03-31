226c05aef694980a9d732776895565216042f793
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-03-31 09:44:23 -0300

    alteração

--- Calculadora.py
+++ Calculadora.py
@@ -1,1 +1,11 @@
-print("ola mundo")
+numb1= float(input("digite o numero: "))
+numb2= float(input("digite o numero: "))
+
+result = numb1+ numb2
+print(f"soma {result:.2f}")
+result = numb1-numb2
+print(f"subtração {result:.2f}")
+result = numb1*numb2
+print(f"multiplicação {result:.2f}")
+result = numb1/ numb2
+print(f"divisão {result:.2f}")
