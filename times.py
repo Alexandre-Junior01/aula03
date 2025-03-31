7b69e81f65d1785007a0322c708d2dd35c10f32e
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-03-31 11:46:51 -0300

    alteração codigo

--- times.py
+++ times.py
@@ -3,8 +3,8 @@ time2 =input('Digite nome do time: ')
 golsTime1 = int(input('digite golstime1: '))
 golsTime2= int(input('digite golstime2: '))
 if golsTime1 > golsTime2:
-    print(f"time {time1} ganhou")
+    print(f"time {time1} ganhou {golsTime1}")
 elif golsTime2>golsTime1:
-    print(f" time {time2} ganhou")
+    print(f" time {time2} ganhou {golsTime2}")
 else:
     print("empate")
