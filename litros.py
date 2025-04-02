cf68ef7df4e80f678a665dd9ee4b54f840081f4e
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-02 09:25:29 -0300

    alteração

--- litros.py
+++ litros.py
@@ -2,13 +2,13 @@
 combust= input('digite E para etanol ou G para gasolina:').upper()
 etanol = 4.90
 gasolina = 5.80
-if combust=="E":
+if combust=="E" or combust=="e":
 
     litros=float(input('digite a quantidade de litros: '))
     print(f'litros gasto {litros*etanol:.2f}')
-elif combust=="G":
+elif combust=="G" or combust=="g":
 
     litros = float(input('digite a quantidade de litros: '))
     print(f'litros gasto {litros*gasolina:.2f}')
 else:
-    print('Digite e ou g')
+    print('Digite e ou g, tipo de combustivel foi errado')
