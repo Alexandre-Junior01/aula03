b786c5fd55abbbf8fdb3827e022e01d41b285bd4
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-07 09:00:28 -0300

    codigo feito

--- Desafio.py
+++ Desafio.py
@@ -1,4 +1,28 @@
 '''problema:
 Dados os valores de horários abaixo, decifre a lógica e faça um código para executar.'''
-   #comentario#
+
+h1 = int(input("digite a hora1: "))
+m1 = int(input("digite a min1: "))
+h2 = int(input("digite a hora2: "))
+m2 = int(input("digite a min2: "))
+
+if h1 > 12:
+   h1= h1-12
+
+if h2 > 12:
+  h2 = h2 - 12
+somaH= h1+ h2
+if somaH >24:
+   somaH = somaH -24
+elif somaH >12:
+    somaH= somaH -12
+print(somaH)
+somaM= m1+ m2
+if somaM>59:
+    somaH = somaH+1
+    somaM= somaM -60
+print(somaH,somaM)
+
+
+
 
